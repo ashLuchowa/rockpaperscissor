64bf65e10c7746a3f6a878f96f2bb8bffbe562fa
Ash Luchowa <ashwinluchowa@gmail.com>
2022-08-18 15:30:26 +0800

    Write game logic

--- js/main.js
+++ js/main.js
@@ -1,5 +1,50 @@
-function getComputerChoice(gameChoice) {
-    return gameChoice[Math.floor(Math.random() * gameChoice.length)];
+// computer chooses between rock, paper or scissor
+const getComputerChoice = () => {
+    const arrOfChoices = ['rock', 'paper', 'scissor']
+    const compChoice = arrOfChoices[Math.floor(Math.random() * 3)]
+    return compChoice;
 }
 
-console.log(getComputerChoice(['rock', 'paper', 'scissor']));
+// playing 1 round
+const playRound = (playerSelection, computerSelection) => {
+
+    // player's choice = rock
+    if (playerSelection === 'rock') {
+        if (computerSelection === 'scissor') {
+            return 'You win!'
+        } else if (computerSelection === 'paper') {
+            return 'You lose!'
+        } else {
+            return 'It\'s a tie!'
+        }
+    }
+
+    // player's choice = paper
+    else if (playerSelection === 'paper') {
+        if (computerSelection === 'rock') {
+            return 'You win!'
+        } else if (computerSelection === 'scissor') {
+            return 'You lose!'
+        } else {
+            return 'It\'s a tie!'
+        }
+    }
+
+    // player's choice = scissor
+    else {
+        if (computerSelection === 'rock') {
+            return 'You lose!'
+        } else if (computerSelection === 'paper') {
+            return 'You win!'
+        } else {
+            return 'It\'s a tie!'
+        }
+    }
+}
+
+const playerSelection = prompt('Choose between rock, paper and scissor', 'rock')
+const computerSelection = getComputerChoice();
+
+console.log(`Player: ${playerSelection}`)
+console.log(`Computer: ${computerSelection}`)
+console.log(playRound(playerSelection, computerSelection))
